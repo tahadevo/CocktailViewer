c7833802abfc6ef86259d7c55b04ebb831185c64
tahadevo <taha.gokcegoz@codevo.com.tr>
2024-09-06 15:08:51 +0300

    Initial Commit

---
+++ CocktailViewer/CocktailViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailViewerApp.swift
+//  CocktailViewer
+//
+//  Created by Taha Emir Gökçegöz on 6.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CocktailViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
